490d59988419a46c6de40548d3bdd99b41ae97fb
fslezak <fslezak@teonite.com>
2021-06-12 15:46:57 +0200

    fix entrypoiny admin

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -4,7 +4,7 @@ import re
 
 UserModel = get_user_model()
 if not UserModel.objects.filter(username='admin').exists():
-  u = UserModel.objects.create_user('admin', password='admin')
+  u = UserModel.objects.create_user(username='admin', password='admin')
   u.is_superuser = True
   u.is_staff = True
   u.save()
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,5 @@
 until nc -z db 5432; do sleep 1; done
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-python3 manage.py shell < create_users.py
+python3 manage.py shell < create_admin.py
 exec "$@"
